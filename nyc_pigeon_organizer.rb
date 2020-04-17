5ee488413934bded4486e7d37686714f044b000a
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 16:13:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
       data_value << value2
     end
     uniq_flatten_data = data_value.flatten.uniq
-    pigeon_list.insert(uniq_flatten_data)
+
     binding.pry
 
   end
