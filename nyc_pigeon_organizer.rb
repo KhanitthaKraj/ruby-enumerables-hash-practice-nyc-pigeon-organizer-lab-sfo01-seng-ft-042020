e214babfc77a0593105017e4c70254bbb3b8ff7d
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 16:55:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,16 +3,16 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data_value = []
   
-  data.each do |key, value|
+  data.each do |attribute, value|
     value.each do |key2, names|
       names.each do |name|
         if !pigeon_list[name]
           pigeon_list[name] = {}
         end
-        if !pigeon_list[name][key]
-          pigeon_list[name][key] = []
+        if !pigeon_list[name][attribute]
+          pigeon_list[name][attribute] = []
         end 
-        pigeon_list[name][key] << key2.to_s
+        pigeon_list[name][attribute] << key2.to_s
       end
     end
     binding.pry
