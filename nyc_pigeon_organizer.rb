7702dddc64e2c8c94966b7304ef322b347651df2
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 17:11:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   
   # iterate over data hash (most-outer hash)
   # attribute refers to :color, :gender, :lives (inner-hashes)
-  # and values refers to 
+  # and values refers to hashs or arrays inside each attribute
   data.each do |attribute, values|
     values.each do |key2, names|
       names.each do |name|
