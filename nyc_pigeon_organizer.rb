f0dc35fb114026cdeee0fb9132012fdf668e3726
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 17:29:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,8 +12,8 @@ def nyc_pigeon_organizer(data)
     values.each do |key2, names|
       # iterate over names-array and while each element inside = |name|
       names.each do |name|
-        # check if name element already exist in pigeon_list-hash
-        # if not yet exist assign that name element into the hash
+        # check if name element already exist in pigeon_list hash
+        # if not yet exist: assign that name element into the hash as a key
         if !pigeon_list[name]
           pigeon_list[name] = {}
         end
