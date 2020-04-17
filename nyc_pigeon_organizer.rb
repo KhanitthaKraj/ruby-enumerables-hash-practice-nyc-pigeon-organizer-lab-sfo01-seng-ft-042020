935ce98396d21c3ea689216030b19466a81c0a5e
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 17:08:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   # iterate over data hash (most-outer hash)
-  # attribute refers to :color
+  # attribute refers to :color, :gender, 
   data.each do |attribute, values|
     values.each do |key2, names|
       names.each do |name|
