d949159ddcb879180a6b868d5d5b103b6f2b3dc6
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 16:32:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name][key]
           pigeon_list[name][key] = []
         end 
-        
+        pigeon_list[name][key] << 
         binding.pry
       end
       
